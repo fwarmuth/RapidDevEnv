e6c745835b789e06929dc6ec360dc7afc9e41619
Felix Warmuth <felix.warmuth@mailbox.org>
2023-12-14 17:38:31 +0100

    Update

---
+++ scripts/setup_dec_container.py
@@ -0,0 +1,63 @@
+import os
+import json
+import subprocess
+import click
+
+@click.command()
+@click.option('--name', help='Specify the name of the container')
+def main(name):
+    # Get the path of the script
+    script_path = os.path.realpath(__file__)
+
+    # Get the path of the devcontainer.json
+    devcon_file = os.path.join(os.path.dirname(script_path), '..', '.devcontainer', 'devcontainer.json')
+
+    if name:
+        # Read devcontainer.json
+        with open(devcon_file, 'r') as f:
+            devcontainer_data = json.load(f)
+        # Change container name
+        devcontainer_data['name'] = name
+        # Write back to devcontainer.json
+        with open(devcon_file, 'w') as f:
+            json.dump(devcontainer_data, f, indent=2)
+        click.echo(f"Changed container name to {name}")
+    else:
+        click.echo("No container name passed, not changing container name.")
+
+    # Set the values for USER_UID and USER_GID
+    new_user_uid = str(os.getuid())
+    new_user_gid = str(os.getgid())
+    update_json_values(devcon_file, 'USER_UID', new_user_uid)
+    update_json_values(devcon_file, 'USER_GID', new_user_gid)
+
+    # Change --hostname to the hostname of the machine
+    container_name = read_json_value(devcon_file, 'name')
+    project_name = f"{container_name}@{os.uname().nodename}"
+    update_json_values(devcon_file, 'PROJECT_NAME', project_name)
+
+    # Ask the user if .git folder should be deleted
+    reply = click.prompt("Do you want to delete the .git folder? (y/n)", default='n')
+    if reply.lower() == 'y':
+        git_folder_path = os.path.join(os.path.dirname(script_path), '..', '.git')
+        if os.path.exists(git_folder_path):
+            # You can uncomment the following line to delete the .git folder
+            # shutil.rmtree(git_folder_path)
+            click.echo("Deleted .git folder")
+        else:
+            click.echo("No .git folder found")
+
+def update_json_values(file_path, key, value):
+    with open(file_path, 'r') as f:
+        devcontainer_data = json.load(f)
+    devcontainer_data[key] = value
+    with open(file_path, 'w') as f:
+        json.dump(devcontainer_data, f, indent=2)
+
+def read_json_value(file_path, key):
+    with open(file_path, 'r') as f:
+        devcontainer_data = json.load(f)
+    return devcontainer_data[key]
+
+if __name__ == "__main__":
+    main()
